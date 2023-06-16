9523995fb8f4657f0a3084b5742f27201a7f437d
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-06-16 13:32:26 +0530

    add: signup api

---
+++ server/routes/auth.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const User = require("../models/user");
+const authRouter = express.Router();
+
+authRouter.post("/api/signup", async (req, res) => {
+  try {
+    const { name, email, profilePic } = req.body;
+    // already exists email
+    let user = await User.findOne({ email: email });
+    if (!user) {
+      user = new User({
+        email: email,
+        profilePic: profilePic,
+        name: name,
+      });
+      user = await user.save();
+    }
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = authRouter;
