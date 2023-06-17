ee7c4325b0836744b2e0c00add5cb81802169988
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-06-18 01:44:30 +0530

    update: title design: doc ui

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const kWhiteColor = Colors.white;
 const kBlackColor = Colors.black;
 const kRedColor = Colors.red;
+var kGreyColor = Colors.grey.shade800;
+const kBlueColor = Color.fromRGBO(26, 115, 232, 1);
--- lib/screens/document_screen.dart
+++ lib/screens/document_screen.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
+import 'package:write_sync/colors.dart';
+import 'package:flutter_quill/flutter_quill.dart' as quill;
+import 'package:write_sync/common/widgets/loader.dart';
+import 'package:write_sync/models/document_model.dart';
+import 'package:write_sync/models/error_model.dart';
+import 'package:write_sync/repository/auth_repository.dart';
+import 'package:write_sync/repository/document_repository.dart';
 
 class DocumentScreen extends ConsumerStatefulWidget {
   final String id;
@@ -10,11 +19,157 @@ class DocumentScreen extends ConsumerStatefulWidget {
 }
 
 class _DocumentScreenState extends ConsumerState<DocumentScreen> {
+  TextEditingController titleController =
+      TextEditingController(text: 'Untitled Document');
+  quill.QuillController? _controller;
+  ErrorModel? errorModel;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDocumentData();
+  }
+
+  void fetchDocumentData() async {
+    errorModel = await ref
+        .read(documentRepositoryProvider)
+        .getDocumentById(ref.read(userProvider)!.token, widget.id);
+
+    if (errorModel!.data != null) {
+      titleController.text = (errorModel!.data as DocumentModel).title;
+      _controller = quill.QuillController(
+        document: errorModel!.data.content.isEmpty
+            ? quill.Document()
+            : quill.Document.fromDelta(
+                quill.Delta.fromJson(errorModel!.data.content),
+              ),
+        selection: const TextSelection.collapsed(offset: 0),
+      );
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+  }
+
+  void updateTitle(WidgetRef ref, String title) {
+    ref.read(documentRepositoryProvider).updateTitle(
+          token: ref.read(userProvider)!.token,
+          id: widget.id,
+          title: title,
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_controller == null) {
+      return const Scaffold(body: Loader());
+    }
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kWhiteColor,
+        elevation: 0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ElevatedButton.icon(
+              onPressed: () {
+                Clipboard.setData(ClipboardData(
+                        text: 'http://localhost:3000/#/document/${widget.id}'))
+                    .then(
+                  (value) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text(
+                          'Link copied!',
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              icon: const Icon(
+                Icons.lock,
+                size: 16,
+              ),
+              label: const Text('Share'),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: kBlueColor, foregroundColor: kWhiteColor),
+            ),
+          ),
+        ],
+        title: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 9.0),
+          child: Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Routemaster.of(context).replace('/');
+                },
+                child: Image.asset(
+                  'assets/images/docs-logo.png',
+                  height: 40,
+                ),
+              ),
+              const SizedBox(width: 10),
+              SizedBox(
+                width: 180,
+                child: TextField(
+                  controller: titleController,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: kBlueColor,
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.only(left: 10),
+                  ),
+                  onSubmitted: (value) => updateTitle(ref, value),
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: kGreyColor,
+                width: 0.1,
+              ),
+            ),
+          ),
+        ),
+      ),
       body: Center(
-        child: Text(widget.id),
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            quill.QuillToolbar.basic(controller: _controller!),
+            const SizedBox(height: 10),
+            Expanded(
+              child: SizedBox(
+                width: 750,
+                child: Card(
+                  color: kWhiteColor,
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: quill.QuillEditor.basic(
+                      controller: _controller!,
+                      readOnly: false,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
