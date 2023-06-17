03696d58ec4c6b5b4924a97db11aa6bcd117e5a5
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-06-17 13:20:48 +0530

    render: data as per current state

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:write_sync/models/error_model.dart';
+import 'package:write_sync/repository/auth_repository.dart';
+import 'package:write_sync/screens/home_screen.dart';
 import 'package:write_sync/screens/login_screen.dart';
 
 void main() async {
@@ -10,18 +13,36 @@ void main() async {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  ErrorModel? errorModel;
+  @override
+  void initState() {
+    getUserData();
+  }
+
+  void getUserData() async {
+    errorModel = await ref.read(authRepositoryProvider).getUserData();
+    if (errorModel != null && errorModel!.data != null) {
+      ref.read(userProvider.notifier).update((state) => errorModel!.data);
+    }
+  }
+
   Widget build(BuildContext context) {
+    final user = ref.watch(userProvider);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: user == null ? const LoginScreen() : const HomeScreen(),
     );
   }
 }
